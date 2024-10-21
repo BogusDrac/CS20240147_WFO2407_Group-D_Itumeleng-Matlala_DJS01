a7df7f71720bed1a9de038a4010d7fa64dd7bfd7
Bogusdrac <bogusdracula7@gmail.com>
2024-10-21 17:34:27 +0200

    managed complexity

--- index.js
+++ index.js
@@ -6,26 +6,27 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const velocityInKmPerHour = 10000; // velocity (km/h)
+const accelerationInMperSecs = 3; // acceleration (m/s^2)
+const timeInSecs = 3600; // seconds (1 hour)
+const distanceInKm = 0; // distance (km)
+const fuelInKg = 5000; // remaining fuel (kg)
+const FuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const newDistance = distanceInKm + ( velocityInKmPerHour * timeInSecs )
+const remainingFuel = FuelBurnRate * timeInSecs
+const newVelocity = calcNewVel(accelerationInMperSecs, velocityInKmPerHour, timeInSecs)
+
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+function calcNewVel (accelerationInMperSecs, velocityInKmPerHour, timeInSecs) { 
+  return velocityInKmPerHour + ( accelerationInMperSecs * timeInSecs )
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Velocity: ${newVelocity} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
